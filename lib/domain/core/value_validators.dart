0de318bba900d5ac74981cc0515058c7fa412589
Ivy-Walobwa <ivywalobwa@gmail.com>
2021-06-13 09:30:11 +0300

    note value validators

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -13,9 +13,25 @@ Either<ValueFailure<String>, String> validateEmailAddress(String input) {
 }
 
 Either<ValueFailure<String>, String> validatePassword(String input) {
-  if (input.length >6) {
+  if (input.length > 6) {
     return right(input);
   } else {
     return left(ValueFailure.shortPassword(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateMaxStringLength(String input, int maxLength) {
+  if(input.length <= maxLength){
+    return right(input);
+  }else{
+    return left(ValueFailure.exceedingLength(failedValue: input, max: maxLength));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input){
+  if(input.isNotEmpty){
+    return right(input);
+  }else{
+    return left(ValueFailure.empty(failedValue: input));
   }
 }

d25cc3a146afd8b4588ab0d4a978b8eeb85ee63b
Ivy-Walobwa <ivywalobwa@gmail.com>
2021-07-11 09:44:25 +0300

    note dismissible widget

--- lib/presentation/notes/notes_overview/widgets/note_container_widget.dart
+++ lib/presentation/notes/notes_overview/widgets/note_container_widget.dart
@@ -6,6 +6,7 @@ import '../../../core/constants.dart';
 
 class NoteContainerWidget extends StatelessWidget {
   final Note note;
+
   const NoteContainerWidget({
     required this.note,
     Key? key,
@@ -15,35 +16,64 @@ class NoteContainerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: klightPinkColor3,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.4),
-                spreadRadius: 6,
-                blurRadius: 10,
-                offset: const Offset(0, 3),
-              ),
-            ],
-          ),
-          child:  Padding(
-            padding:
-            const EdgeInsets.all( kappPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:  [
-                Text(
-                  note.noteBody.getOrCrash(),
-                  style: Theme.of(context).textTheme.headline2!.copyWith(color: Colors.black),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
+        InkWell(
+          onTap: (){
+            // TODO: Navigate to note detail
+            print('tapped');
+          },
+          child: Dismissible(
+            key: Key(note.id.getOrCrash()),
+            direction: DismissDirection.endToStart,
+            confirmDismiss: (DismissDirection direction) async {
+              // TODO: Implement delete alert dialog
+              print('delete');
+            },
+            onDismissed: (DismissDirection direction) {
+              // TODO: Implement delete
+              print('delete');
+            },
+            background: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
                 ),
-                kverticalSpaceBetweenWidgets,
-                 Text(dateTimeToHourMinute(note.noteTimeStamp.getOrCrash())),
               ],
+            ),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: klightPinkColor3,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 6,
+                    blurRadius: 10,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(kappPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      note.noteBody.getOrCrash(),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline2!
+                          .copyWith(color: Colors.black),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    kverticalSpaceBetweenWidgets,
+                    Text(dateTimeToHourMinute(note.noteTimeStamp.getOrCrash())),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
@@ -53,8 +83,8 @@ class NoteContainerWidget extends StatelessWidget {
   }
 
   String dateTimeToHourMinute(String date) {
-   final DateFormat dateFormat = DateFormat.Hm();
-   final DateTime dateTime = DateTime.parse(date);
+    final DateFormat dateFormat = DateFormat.Hm();
+    final DateTime dateTime = DateTime.parse(date);
     return dateFormat.format(dateTime);
-  }}
-
+  }
+}

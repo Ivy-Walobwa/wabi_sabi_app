2e35b9f0de528fd2fe25111db45d93b76bd83b56
Ivy-Walobwa <ivywalobwa@gmail.com>
2021-07-24 14:32:06 +0300

    added saving indicator

--- lib/presentation/notes/note_form/widgets/edit_note_form_widget.dart
+++ lib/presentation/notes/note_form/widgets/edit_note_form_widget.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 import '../../../../application/notes/note_form/note_form_bloc.dart';
 import '../../../../domain/notes/value_objects.dart';
 import '../../../core/constants.dart';
-import 'top_form_row_widget.dart';
+import 'widgets.dart';
 
 class EditNoteFormWidget extends StatelessWidget {
 
@@ -17,89 +17,93 @@ class EditNoteFormWidget extends StatelessWidget {
       height: height * 0.96,
       child: BlocBuilder<NoteFormBloc, NoteFormState>(
         builder: (context, state) {
-          return Form(
-            autovalidateMode: state.showError
-                ? AutovalidateMode.always
-                : AutovalidateMode.disabled,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(kappPadding),
-                  child: Column(
-                    children: [
-                      const TopFormRowWidget( text: 'Edit Note',),
-                      kverticalSpaceBetweenWidgets,
-                      kverticalSpaceBetweenWidgets,
-                      Text(dateTimeToTodayDate(state.note.noteTimeStamp.getOrCrash()),),
-                      Text(dateTimeToHourMinute(state.note.noteTimeStamp.getOrCrash())),
-                      kverticalSpaceBetweenWidgets,
-                      TextFormField(
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(color: Colors.black),
-                        initialValue: state.note.noteBody.getOrCrash(),
-                        cursorColor: kpurpleColor,
-                        minLines: (height*0.015).toInt(),
-                        maxLines: null,
-                        maxLength: NoteBody.maxLength,
-                        decoration: const InputDecoration(
-                          counterText: '',
-                          filled: true,
-                          fillColor: klightPinkColor2,
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: klightPinkColor1),
+          return Stack(
+            children: [
+              Form(
+                autovalidateMode: state.showError
+                    ? AutovalidateMode.always
+                    : AutovalidateMode.disabled,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(kappPadding),
+                      child: Column(
+                        children: [
+                          const TopFormRowWidget( text: 'Edit Note',),
+                          kverticalSpaceBetweenWidgets,
+                          kverticalSpaceBetweenWidgets,
+                          Text(dateTimeToTodayDate(state.note.noteTimeStamp.getOrCrash()),),
+                          Text(dateTimeToHourMinute(state.note.noteTimeStamp.getOrCrash())),
+                          kverticalSpaceBetweenWidgets,
+                          TextFormField(
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(color: Colors.black),
+                            initialValue: state.note.noteBody.getOrCrash(),
+                            cursorColor: kpurpleColor,
+                            minLines: (height*0.015).toInt(),
+                            maxLines: null,
+                            maxLength: NoteBody.maxLength,
+                            decoration: const InputDecoration(
+                              counterText: '',
+                              filled: true,
+                              fillColor: klightPinkColor2,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: klightPinkColor1),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: klightPinkColor2),
+                              ),
+                            ),
+                            onChanged: (value) => context
+                                .read<NoteFormBloc>()
+                                .add(NoteFormEvent.noteBodyChanged(value)),
+                            validator: (_) => context
+                                .read<NoteFormBloc>()
+                                .state
+                                .note
+                                .noteBody
+                                .value
+                                .fold(
+                                    (f) => f.maybeMap(
+                                    exceedingLength: (f) =>
+                                    'Exceeding Length, max: ${f.max}',
+                                    empty: (_) => 'Note cannot be empty.',
+                                    orElse: () => null),
+                                    (_) => null),
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: klightPinkColor2),
-                          ),
-                        ),
-                        onChanged: (value) => context
-                            .read<NoteFormBloc>()
-                            .add(NoteFormEvent.noteBodyChanged(value)),
-                        validator: (_) => context
-                            .read<NoteFormBloc>()
-                            .state
-                            .note
-                            .noteBody
-                            .value
-                            .fold(
-                                (f) => f.maybeMap(
-                                exceedingLength: (f) =>
-                                'Exceeding Length, max: ${f.max}',
-                                empty: (_) => 'Note cannot be empty.',
-                                orElse: () => null),
-                                (_) => null),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                InkWell(
-                  onTap: state.isBodyChanged
-                      ? () {
-                    context
-                        .read<NoteFormBloc>()
-                        .add(const NoteFormEvent.saved());
-                  }
-                      : null,
-                  child: Container(
-                    padding: const EdgeInsets.all(kappPadding * 1.4),
-                    decoration: BoxDecoration(
-                      color: state.isBodyChanged ? kpurpleColor : Colors.grey,
-                      borderRadius:
-                      const BorderRadius.only(topLeft: Radius.circular(10)),
                     ),
-                    child: const Icon(
-                      Icons.done,
-                      size: 40,
-                      color: klightPinkColor2,
+                    InkWell(
+                      onTap: state.isBodyChanged
+                          ? () {
+                        context
+                            .read<NoteFormBloc>()
+                            .add(const NoteFormEvent.saved());
+                      }
+                          : null,
+                      child: Container(
+                        padding: const EdgeInsets.all(kappPadding * 1.4),
+                        decoration: BoxDecoration(
+                          color: state.isBodyChanged ? kpurpleColor : Colors.grey,
+                          borderRadius:
+                          const BorderRadius.only(topLeft: Radius.circular(10)),
+                        ),
+                        child: const Icon(
+                          Icons.done,
+                          size: 40,
+                          color: klightPinkColor2,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SavingIndicatorWidget(isSaving: state.isSaving,)            ],
           );
         },
       ),
@@ -117,3 +121,4 @@ class EditNoteFormWidget extends StatelessWidget {
     return dateFormat.format(dateTime);
   }
 }
+
--- lib/presentation/notes/note_form/widgets/widgets.dart
+++ lib/presentation/notes/note_form/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'edit_note_form_widget.dart';
 export 'lead_on_text_view_widget.dart';
 export 'new_note_form_widget.dart';
 export 'top_form_row_widget.dart';
+export 'saving_indicator_widget.dart';

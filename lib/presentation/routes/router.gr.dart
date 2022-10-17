6bf034172adcab6c3ac07a6b15d1129cf48bea5b
Ivy Walobwa <ivywalobwa@gmail.com>
2022-10-17 16:15:26 +0300

    Upgrade most packages

--- lib/presentation/routes/router.gr.dart
+++ lib/presentation/routes/router.gr.dart
@@ -1,59 +1,96 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
 // AutoRouteGenerator
 // **************************************************************************
+//
+// ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i1;
-import 'package:flutter/material.dart' as _i2;
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:flutter/material.dart' as _i3;
 
-import '../presentation.dart' as _i3;
+import '../presentation.dart' as _i1;
 
-class WabiRouter extends _i1.RootStackRouter {
-  WabiRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+class WabiRouter extends _i2.RootStackRouter {
+  WabiRouter([_i3.GlobalKey<_i3.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {
-    SplashRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+  final Map<String, _i2.PageFactory> pagesMap = {
+    SplashRoute.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
         routeData: routeData,
-        builder: (_) {
-          return _i3.SplashPage();
-        }),
-    SignInRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        child: _i1.SplashPage(),
+      );
+    },
+    SignInRoute.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
         routeData: routeData,
-        builder: (_) {
-          return _i3.SignInPage();
-        }),
-    TabView.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        child: _i1.SignInPage(),
+      );
+    },
+    TabView.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
         routeData: routeData,
-        builder: (_) {
-          return _i3.TabView();
-        })
+        child: _i1.TabView(),
+      );
+    },
   };
 
   @override
-  List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(SplashRoute.name, path: '/'),
-        _i1.RouteConfig(SignInRoute.name, path: '/sign-in-page'),
-        _i1.RouteConfig(TabView.name, path: '/tab-view')
+  List<_i2.RouteConfig> get routes => [
+        _i2.RouteConfig(
+          SplashRoute.name,
+          path: '/',
+        ),
+        _i2.RouteConfig(
+          SignInRoute.name,
+          path: '/sign-in-page',
+        ),
+        _i2.RouteConfig(
+          TabView.name,
+          path: '/tab-view',
+        ),
       ];
 }
 
-class SplashRoute extends _i1.PageRouteInfo {
-  const SplashRoute() : super(name, path: '/');
+/// generated route for
+/// [_i1.SplashPage]
+class SplashRoute extends _i2.PageRouteInfo<void> {
+  const SplashRoute()
+      : super(
+          SplashRoute.name,
+          path: '/',
+        );
 
   static const String name = 'SplashRoute';
 }
 
-class SignInRoute extends _i1.PageRouteInfo {
-  const SignInRoute() : super(name, path: '/sign-in-page');
+/// generated route for
+/// [_i1.SignInPage]
+class SignInRoute extends _i2.PageRouteInfo<void> {
+  const SignInRoute()
+      : super(
+          SignInRoute.name,
+          path: '/sign-in-page',
+        );
 
   static const String name = 'SignInRoute';
 }
 
-class TabView extends _i1.PageRouteInfo {
-  const TabView() : super(name, path: '/tab-view');
+/// generated route for
+/// [_i1.TabView]
+class TabView extends _i2.PageRouteInfo<void> {
+  const TabView()
+      : super(
+          TabView.name,
+          path: '/tab-view',
+        );
 
   static const String name = 'TabView';
 }
